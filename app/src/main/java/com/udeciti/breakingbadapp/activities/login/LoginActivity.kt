30fcd1d8af1098ac61166d03e9a197f3c648bca8
Edgar Campos <edgargcg@gmail.com>
2021-08-29 14:29:57 -0500

    Login page logic defined

--- app/src/main/java/com/udeciti/breakingbadapp/activities/login/LoginActivity.kt
+++ app/src/main/java/com/udeciti/breakingbadapp/activities/login/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.udeciti.breakingbadapp.activities.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.udeciti.breakingbadapp.R
+import com.udeciti.breakingbadapp.activities.main.MainActivity
 import com.udeciti.breakingbadapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -11,6 +14,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login)
 
-        
+        val email = binding.email.text
+        val password = binding.password.text
+
+        binding.loginButton.setOnClickListener(){
+            if (
+                email.isEmpty()
+                ||
+                password.isEmpty()
+            ){
+                Toast.makeText(this, "Please type your email and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/udeciti/breakingbadapp/activities/main/MainActivity.kt
+++ app/src/main/java/com/udeciti/breakingbadapp/activities/main/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.udeciti.breakingbadapp
+package com.udeciti.breakingbadapp.activities.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udeciti.breakingbadapp.R
+import com.udeciti.breakingbadapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val EMAIL_KEY = "email"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+
     }
 }
