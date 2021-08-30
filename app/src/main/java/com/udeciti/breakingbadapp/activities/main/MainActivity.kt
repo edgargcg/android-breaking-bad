668020aaaaf45ed8565216694a1fcf9e53eacd64
Edgar Campos <edgargcg@gmail.com>
2021-08-30 12:05:48 -0500

    AppBarLayout, TabLayout and ViewPager2 created and configured

--- app/src/main/java/com/udeciti/breakingbadapp/activities/main/MainActivity.kt
+++ app/src/main/java/com/udeciti/breakingbadapp/activities/main/MainActivity.kt
@@ -3,8 +3,14 @@ package com.udeciti.breakingbadapp.activities.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.udeciti.breakingbadapp.R
 import com.udeciti.breakingbadapp.databinding.ActivityMainBinding
+import com.udeciti.breakingbadapp.fragments.CharactersFragment
+import com.udeciti.breakingbadapp.fragments.PhraseFragment
+import com.udeciti.breakingbadapp.fragments.SettingsFragment
+import com.udeciti.breakingbadapp.fragments.adapters.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -14,7 +20,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val tabLayout = binding.tabLayout
+        val viewPager = binding.viewPager
+        val fragments = mutableListOf<Fragment>(
+            CharactersFragment(),
+            PhraseFragment(),
+            SettingsFragment()
+        )
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle, fragments)
 
+        viewPager.adapter = viewPagerAdapter
+        TabLayoutMediator(tabLayout, viewPager){
+            tab, position ->
+            when(position){
+                0 -> tab.text = "Characters"
+                1 -> tab.text = "Phrase"
+                2 -> tab.text = "Settings"
+            }
+        }.attach()
 
     }
 }
